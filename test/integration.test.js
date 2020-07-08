bc23aa103cb8e520c0c1b4499ffef1dff2aeab99
Steven Degutis <sdegutis@atlasauthority.com>
2020-07-08 15:02:25 -0500

    More Jest configuration.

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,3 +1,25 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getUri();
+  const opts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  await mongoose.connect(mongoUri, opts, (err) => {
+    if (err) console.error(err);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
 test("basics", () => {
   expect(1 + 1).toBe(2);
 });
