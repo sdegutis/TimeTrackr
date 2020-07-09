173e82e27915b502a329dbeaed3843fc2013eabd
Steven Degutis <sdegutis@atlasauthority.com>
2020-07-09 10:43:46 -0500

    Trying out UIKit.

--- client/app.js
+++ client/app.js
@@ -5,7 +5,9 @@ const html = htm.bind(React.createElement);
 
 export const Timer = ({ count }) => {
   return html`
-    <p>the time = <b>${count}</b></p>
+    <div uk-alert="">
+      <p>the time = <b>${count}</b></p>
+    </div>
   `;
 };
 
@@ -19,7 +21,31 @@ export const App = () => {
   }, []);
 
   return html`
-    <h2>Hello world</h2>
-    <${Timer} count=${count} />
+    <nav class="uk-navbar-container" uk-navbar="">
+        <div class="uk-navbar-left">
+
+            <ul class="uk-navbar-nav">
+                <li class="uk-active"><a href="#">Active</a></li>
+                <li>
+                    <a href="#">Parent</a>
+                    <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                            <li class="uk-active"><a href="#">Active</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li><a href="#">Item</a></li>
+            </ul>
+
+        </div>
+    </nav>
+
+    <div class="uk-container">
+
+      <h2>Hello world</h2>
+      <${Timer} count=${count} />
+    </div>
   `;
 };
