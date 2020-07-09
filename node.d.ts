dfa63c1b6f30b7d51553834b0cd13496434d3ee9
Steven Degutis <sdegutis@atlasauthority.com>
2020-07-09 11:57:30 -0500

    Type-checking client-side code.

---
+++ node.d.ts
@@ -0,0 +1,12 @@
+declare module "https://unpkg.com/es-react" {
+  import * as React from 'react';
+  import * as ReactDOM from 'react-dom';
+  export { React, ReactDOM };
+}
+
+declare module "https://unpkg.com/htm?module" {
+  import htm from 'htm';
+  export default htm;
+}
+
+declare var jwt_decode: (str: string) => object;
