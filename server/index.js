adb9b1ae5760d3027c9991bb91088c6f3b74d40e
Steven Degutis <sdegutis@atlasauthority.com>
2020-07-10 09:11:35 -0500

    Cleaning up code.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
-require('./db').whenReady.then(() => {
-  const port = process.env.NODE_PORT;
-  const { app } = require('./app');
+const { app } = require('./app');
+const db = require('./db');
+
+const port = process.env.NODE_PORT;
+
+db.whenReady.then(() => {
   app.listen(port, function () {
     console.log('Listening on %s', port);
   });
