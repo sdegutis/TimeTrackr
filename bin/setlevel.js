36f7ca3d64ee4c6e744b2728648c183ca1b59761
Steven Degutis <sdegutis@atlasauthority.com>
2020-07-10 16:00:45 -0500

    Fixing binary.

--- bin/setlevel.js
+++ bin/setlevel.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 const [, , email, role] = process.argv;
-if (!email || !role || !['user', 'manager', 'admin'].includes(role))
-  return console.log('Usage: promote.js <email> (user | manager | admin)');
+if (!email || !role || !['user', 'manager', 'admin'].includes(role)) {
+  console.log('Usage: promote.js <email> (user | manager | admin)');
+  process.exit(1);
+}
 
-const { User } = require("../server/user");
+const { User } = require("../server/model/user");
 console.log(`Promoting ${email} to ${role}`);
 require('../server/db').whenReady.then(() => {
   User.findOne({ email }).then((user) => {
-    user.setAuthLevel(role);
+    user.setAuthLevel(/** @type{*} */(role));
     user.save().then(() => {
       console.log("Done.");
       process.exit(0);
